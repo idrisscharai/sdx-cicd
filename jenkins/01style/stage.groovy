94ad0d8d466ef80d0e4ed7677280584f8eb8740a
abuharis <abuharis709@gmail.com>
2020-08-12 17:06:56 +0300

    Update stage.groovy
    Deprectated warning funcationality

--- jenkins/01style/stage.groovy
+++ jenkins/01style/stage.groovy
@@ -13,16 +13,6 @@ def main()
   try {
     sh "flake8 ./${params.APPSRCDIR}/app/ | sed 's|^./*||' > flake8.log"
     sh "cat ./flake8.log"
-
-    step([
-      $class: 'WarningsPublisher',
-      parserConfigurations: [[
-        parserName: 'Pep8',
-        pattern: '**/flake8.log'
-      ]],
-      unstableTotalAll: '0',
-      usePreviousBuildAsReference: true
-    ])
   }
 	catch (e) {
 		echo 'ERROR: ' + e.toString()
